8f70893f064661a037cd03ab80c0045c5669b063
Erik Fjetland <efjetland@gmail.com>
2017-02-17 01:17:03 +0100

    Finished Assignment 4b

--- Assignment 4b/script.js
+++ Assignment 4b/script.js
@@ -8,7 +8,6 @@ function init(){
   searchBox.oninput = function(){
     search();
   }
-  console.log("init");
 }
 
 function entry(name, tlf, email){
@@ -16,43 +15,79 @@ function entry(name, tlf, email){
   this.tlf = tlf;
   this.email = email;
   this.visible = true;
-  console.log("Create contact");
 }
 
 function createContact(){
-  console.log("create");
-  var name = document.getElementById("nameInput").value;
-  var tlf = document.getElementById("tlfInput").value;
-  var email = document.getElementById("emailInput").value;
-  switch(validate(name,tlf,email)){
-    case 1:
+  var name = document.getElementById("nameInput").value.trim();
+  var tlf = document.getElementById("tlfInput").value.trim();
+  var email = document.getElementById("emailInput").value.trim();
+  if(validate(name,tlf,email)){
       contacts.push(new entry(name, tlf, email));
-      drawContacts();
-      break;
-    case 2:
-      alert("Please enter a valid name");
-      break;
-    case 3:
-      alert("Please enter a valid number");
-      break;
-    case 4:
-      alert("Please enter a valid email adress");
-      break;
-    default:
-      alert("Something went wrong");
+      search();
   }
-
 }
+
+function editContact(contact){
+  var form = document.getElementById("contactForm");
+  var entry = contacts[parseInt(contact.id.match(/\d+/)[0])];
+  var addButton = document.getElementById("toggleForm");
+  addButton.classList.remove('fa-minus-square');
+  addButton.classList.add('fa-plus-square');
+
+  form.style.display = 'block';
+  form.children[0].innerHTML = "Edit Contact";
+  form.children[2].value = contact.children[0].innerHTML;
+  form.children[4].value = contact.children[1].innerHTML;
+  form.children[6].value = contact.children[2].text;
+  form.children[7].onclick = function(){
+    var name = form.children[2].value.trim();
+    var tlf = form.children[4].value.trim();
+    var email = form.children[6].value.trim();
+    if(validate(name,tlf,email)){
+      contact.children[0].innerHTML = name;
+      entry.name = name;
+      contact.children[1].innerHTML = tlf;
+      entry.tlf = tlf;
+      contact.children[2].text = email;
+      contact.children[2].setAttribute("href","mailto:" + email);
+      entry.email = email;
+      search();
+    }
+  };
+}
+function deleteContact(contact){
+  if(confirm("Are you sure you wish to delete "+contact.children[0].innerHTML+" from your contacts?")){
+    var index = parseInt(contact.id.match(/\d+/)[0]);
+    contacts.splice(index,1);
+    search();
+    toggleInput();
+  }
+}
+
 
 function toggleInput(){
   var button = document.getElementById("toggleForm");
-  var contactForm = document.getElementById("contactForm");
-  contactForm.style.display = 'none';
+  var form = document.getElementById("contactForm");
+  form.style.display = 'none';
+  button.classList.remove('fa-minus-square');
+  button.classList.add('fa-plus-square');
   button.onclick = function() {
-    console.log(contactForm.style.display);
-    if(contactForm.style.display === 'none'){
-      contactForm.style.display = 'block';
-    } else contactForm.style.display = 'none';
+    if(form.style.display === 'none' || form.children[0].innerHTML === "Edit Contact"){
+      button.classList.add('fa-minus-square');
+      button.classList.remove('fa-plus-square');
+      form.children[0].innerHTML = "Add Contact"
+      form.children[2].value = '';
+      form.children[4].value = '';
+      form.children[6].value = '';
+      form.children[7].onclick = function(){
+        createContact();
+      }
+      form.style.display = 'block';
+    } else{
+      button.classList.remove('fa-minus-square');
+      button.classList.add('fa-plus-square');
+      form.style.display = 'none';
+    }
   };
 }
 
@@ -70,7 +105,6 @@ function drawContacts(){
     if(!contacts[i].visible){
       continue;
     }
-    console.log("asdaf");
     var contactName = document.createElement("P");
     contactName.appendChild(document.createTextNode(contacts[i].name));
     contactName.className = "contact-name";
@@ -81,39 +115,50 @@ function drawContacts(){
     contactEmail.appendChild(document.createTextNode(contacts[i].email));
     contactEmail.className = "contact-email";
     contactEmail.setAttribute("href","mailto:"+contacts[i].email)
+    var edit = document.createElement("a")
+    edit.className = "fa fa-pencil clickable";
+    edit.onclick = function(){
+      editContact(this.parentElement);
+    }
+    var del = document.createElement("a");
+    del.className = "fa fa-trash clickable";
+    del.onclick = function(){
+      deleteContact(this.parentElement);
+    }
     var newContact = document.createElement("div");
     newContact.className = "contact";
+    newContact.id = "contact-"+i;
     newContact.appendChild(contactName);
     newContact.appendChild(contactTlf);
     newContact.appendChild(contactEmail);
+    newContact.appendChild(edit);
+    newContact.appendChild(del);
     contactList.appendChild(newContact);
   }
 }
 
 function validate(name, tlf, email){
   var namePattern = /[^abcdefghijklmnopqrstuvwxyzæøå]/ig;
-  var tlfPattern = /[^0-9()+- ]/g;
-  var emailLocalPattern = /[^a-z!#$%&'*+-/=?^_`{|}~]+[0-9]/ig;
-  var emailDomainPattern = /[0-9a-z.-]/ig;
+  var tlfPattern = /[^0-9()+-\s]/g;
+  var emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if(namePattern.test(name)){
-    return 2;
+    alert("Please enter a valid name");
+    return false;
   }
-  if(tlfPattern.test(tlf)){
-    return 3;
+  if(tlfPattern.test(tlf) && email == ""){
+    alert("Please enter a valid number");
+    return false;
   }
-  var splitMail = email.split('@');
-  if(splitMail.length !== 2){
-    return 4;
+    if(!emailPattern.test(email) && tlf == ""){
+    alert("Please enter a valid email adress");
+    return false;
   }
-
-  if(emailLocalPattern.test(splitMail[0]) && emailDomainPattern.test(splitMail[1])){
-    return 4;
-  }
-  return 1;
+  return true;
 }
 
 function search(){
   var search = document.getElementById("searchBox").value.trim();
+  search = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
   var searchExp = new RegExp(search,"i")
   if(search == ""){
     for(var i = 0;i<contacts.length;i++){
